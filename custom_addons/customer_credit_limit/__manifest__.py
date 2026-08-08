e2478d1c4d8271492ece8e1e1f8ced932a26ebf1
NareshKumar11 <nareshmca.p@gmail.com>
2026-08-08 16:16:43 +0530

    Update customer credit limit manifest

--- custom_addons/customer_credit_limit/__manifest__.py
+++ custom_addons/customer_credit_limit/__manifest__.py
@@ -21,7 +21,7 @@ Works with no configuration. Six settings in Accounting > Settings.
     'author': '',
     'website': '',
     'license': 'LGPL-3',
-    'depends': ['sale_management', 'account', 'stock'],kdfkl;dkfldzlfmldaflmfdl
+    'depends': ['sale_management', 'account', 'stock'],
     'data': [
         'security/credit_security.xml',
         'security/ir.model.access.csv',
@@ -33,7 +33,7 @@ Works with no configuration. Six settings in Accounting > Settings.
         'views/sale_order_views.xml',
         'views/credit_approval_views.xml',
         'views/credit_log_views.xml',
-        'views/menus.xml',kjdsfkdkfnkdnfkdnafkldafn
+        'views/menus.xml',
     ],
     'installable': True,
     'application': False,
